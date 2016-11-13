9d4eee506ab3be8fae42e42f223f3a0d1ba2079e
J Alvin <i_dever@hotmail.com>
2016-11-13 16:10:16 +0800

    initial

---
+++ WebApplication/Controllers/AppsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class AppsController : Controller
+    {
+        // GET: Apps
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
